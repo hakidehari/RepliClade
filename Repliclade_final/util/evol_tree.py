5d2d9878262639277ccadb6e3d41dbe478aa0f4f
hakidehari <38669902+hakidehari@users.noreply.github.com>
2021-01-29 17:05:39 -0600

    commit

--- Repliclade_final/util/evol_tree.py
+++ Repliclade_final/util/evol_tree.py
@@ -5,15 +5,21 @@ class TreeNode(object):
         self.seq = seq
         self.children = []
         self.is_extinct = False
+        self.parent = None
 
 
     def add_children(self, sequences):
         if not self.is_extinct:
             if isinstance(sequences, list):
                 self.children = [TreeNode(seq) for seq in sequences]
+                for child in self.children
             else:
                 print("No List provided for TreeNode.add_children function.")
 
     
     def make_extinct(self):
         self.is_extinct = True
+
+    
+    def get_parent(self):
+        return self.parent
